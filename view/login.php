8330fb6d8cc379568ee3db479fe6532b83d3d63e
Julio Choquehuayta <juliosh10@gmail.com>
2022-12-05 12:23:35 -0500

    modificacion login

--- view/login.php
+++ view/login.php
@@ -9,38 +9,36 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 
-<body id="login-user">
-    <header>
-        <div class="navbar align-items-center justify-content-center" id="cabecera-login">
-        <h2>APLICACIÓN DE GUÍA ACADÉMICA</h2>    
-        </div>
-    </header>
+<body id="login-user" class="text-center">
+    
 
-    <div id="login" class="container-md text-center">
-        <br>
-        <div id="login-formulario" class="row">
+    <main id="login" class="form-signin w-100 m-auto">
+        
 
-            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
-                <div class="form-group">
-                    <div id="login-icono">
-                        <img src="view/img/person-circle.svg" alt="mdo" width="200" height="200" class="rounded-circle">
-                    </div>
-                    <h4 id="login-titulo">Inicio de Sesión</h4>
-                    <div class="login-entrada">
-                        <label for="login-usuario">Usuario:</label><br>
-                        <input id="login-usuario" class="login-txt" name="usuario" type="text" placeholder="Usuario" required>
-                    </div>
-                    <div class="login-entrada">
-                        <label for="login-contrasenia">Contraseña:</label><br>
-                        <input id="login-contra" class="login-txt" name="contra" type="password" placeholder="Contraseña" required>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="recordar-usuario">
-                        <label for="recordar-usuario">Recordar Usuario</label>
-                    </div>
-                    <input class="btn btn-primary" type="submit" name="iniciar-sesion" value="Iniciar Sesión">
-                </div>
-            </form>
-        </div>
-    </div>
+
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+
+
+            <img src="view/img/person-circle.svg" alt="mdo" width="100px" height="100px" class="rounded-circle">
+
+            <h4 id="login-titulo" class="h3 mb-3 fw-normal">Inicio de Sesión</h4>
+            <div class="form-floating">
+                <label for="login-usuario">Usuario:</label><br>
+                <input id="login-usuario" class="form-control" name="usuario" type="text" placeholder="Usuario" required>
+            </div>
+            <div class="form-floating">
+                <label for="login-contrasenia">Contraseña:</label><br>
+                <input id="login-contra" class="form-control" name="contra" type="password" placeholder="Contraseña" required>
+            </div>
+            <div class="checkbox mb-3">
+                <input type="checkbox" id="recordar-usuario">
+                <label for="recordar-usuario">Recordar Usuario</label>
+            </div>
+            <input class="w-100 btn btn-lg btn-primary" type="submit" name="iniciar-sesion" value="Iniciar Sesión">
+
+        </form>
+
+    </main>
+</body>
+
 </html>
